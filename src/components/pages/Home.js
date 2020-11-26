d2306febc8794cc9d2b1fd0075576a0444a54416
krishlahoti <krishlahoti@gmail.com>
2020-11-26 09:09:08 +0530

    initial commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {PageLayout} from 'components/common';
+
+export default function Home(){
+    return (
+        <PageLayout>
+            <h1>
+                Home
+            </h1>
+            <p>
+
+              I'm baby sustainable typewriter glossier jean shorts. Slow-carb chicharrones cliche, art party hella narwhal echo park marfa try-hard flexitarian. Leggings franzen lumbersexual brunch organic you probably haven't heard of them single-origin coffee paleo deep v four loko taxidermy master cleanse
+
+            </p>
+        </PageLayout>
+    )
+}
